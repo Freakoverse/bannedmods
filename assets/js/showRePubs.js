81cbaa0732b535ab840ae84b65df1edf702a3497
Freakoverse <136988523+Freakoverse@users.noreply.github.com>
2024-10-31 14:56:21 +0300

    Add files via upload

--- assets/js/showRePubs.js
+++ assets/js/showRePubs.js
@@ -19,10 +19,11 @@ function handleActionsButtonClick(event) {
 
 // Function to handle the click event on the sMI2GBIBRepubTopBtn
 function handleRepubTopBtnClick(event) {
-    // Check if the clicked element matches the button class
-    if (event.target.matches('.sMI2GBIBRepubTopBtn')) {
+    // Check if the clicked element is the button or a child of the button
+    const button = event.target.closest('.sMI2GBIBRepubTopBtn');
+    if (button) {
         // Find the closest secMainInside2GridBoxInsideBox parent
-        const parentBox = event.target.closest('.secMainInside2GridBoxInsideBox');
+        const parentBox = button.closest('.secMainInside2GridBoxInsideBox');
 
         if (parentBox) {
             // Find the sMI2GBIBRepub element within the same parent
