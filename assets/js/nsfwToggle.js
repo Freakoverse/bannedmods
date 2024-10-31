d679fd6070239e2eb838c0025489b03f1ece2e69
Freakoverse <136988523+Freakoverse@users.noreply.github.com>
2024-10-31 15:17:57 +0300

    Add files via upload

--- assets/js/nsfwToggle.js
+++ assets/js/nsfwToggle.js
@@ -5,23 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const elementsToToggle = document.querySelectorAll('.secMainInside2GridBoxInsideBox');
         
         elementsToToggle.forEach(function(element) {
-            if (checkbox.checked) {
-                // Show the element if it has the class sMI2GBIBFeaturedTopNSFWShow
                 if (element.querySelector('.sMI2GBIBFeaturedTopNSFWShow')) {
                     element.style.filter = 'unset'; // Show the element
-                    element.style.opacity = '0.25;'
+                    element.style.opacity = '1'; // Fully visible
                 } else {
                     element.style.filter = 'unset'; // Keep other elements visible
-                    element.style.opacity = '0.25;'
+                    element.style.opacity = '1'; // Fully visible
                 }
             } else {
                 // Hide only elements with the class sMI2GBIBFeaturedTopNSFWShow
                 if (element.querySelector('.sMI2GBIBFeaturedTopNSFWShow')) {
                     element.style.filter = 'blur(10px)'; // Hide the element
-                    element.style.opacity = '0.25;'
+                    element.style.opacity = '0.25'; // Partially visible
                 } else {
                     element.style.filter = 'unset'; // Keep other elements visible
-                    element.style.opacity = '1;'
+                    element.style.opacity = '1'; // Fully visible
                 }
             }
         });
